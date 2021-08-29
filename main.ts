9a39147777c2854c18759f385572c34488ebea8f
RINCONALEX <89745460+RINCONALEX@users.noreply.github.com>
2021-08-29 14:05:02 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,8 +12,14 @@ let AFORO = 0
 AFORO = 0
 basic.forever(function () {
     basic.showNumber(AFORO)
-    if (AFORO >= 10) {
-        basic.showIcon(IconNames.Skull)
-        music.playMelody("C - C - C - C - ", 120)
+    if (true) {
+        if (AFORO >= 5) {
+            basic.showIcon(IconNames.Skull)
+            music.playMelody("C - C - C - C - ", 120)
+        } else {
+            basic.showIcon(IconNames.Heart)
+            music.playMelody("A - C5 C5 - - - - ", 300)
+        }
+        basic.pause(5000)
     }
 })
